9766f56a8e2248796a001708ed52e016be438a3c
Diogo Sales <diogosalesdev@gmail.com>
2021-11-21 18:43:06 -0300

    Services created and app configured

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,58 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import {
+  Controller,
+  Get,
+  Param,
+  Post,
+  Body,
+  Put,
+  Delete,
+} from '@nestjs/common';
+import { ScheduleService } from './schedule.service';
+import { Schedule as ScheduleModel } from '@prisma/client';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly scheduleService: ScheduleService,
+  ) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('schedule/:id')
+  async getScheduleById(@Param('id') id: string): Promise<ScheduleModel> {
+    return this.scheduleService.schedule({ id: id });
+  }
+
+  @Get('schedule')
+  async getSchedules(): Promise<ScheduleModel[]> {
+    return this.scheduleService.schedules({});
+  }
+
+  @Post('schedule')
+  async createSchedule(
+    @Body() postData: { name: string; email: string; date_schedule: string, hour_schedule: string },
+  ): Promise<ScheduleModel> {
+    const { name, email, date_schedule, hour_schedule } = postData;
+    return this.scheduleService.newSchedule({
+      name,
+      email,
+      date_schedule,
+      hour_schedule
+    });
+  }
+
+  @Put('schedule/:id')
+  async putSchedule(
+    @Param('id') id: string,
+    @Body() postData: { name?: string; email?: string; date_schedule?: string; hour_schedule: string; },
+  ): Promise<ScheduleModel> {
+    const data = postData
+    return this.scheduleService.updateSchedule({
+      data,
+      where: { id: id },
+    });
+  }
+
+  @Delete('post/:id')
+  async deleteSchedule(@Param('id') id: string): Promise<ScheduleModel> {
+    return this.scheduleService.eraseSchedule({ id: id });
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
+import { PrismaService } from 'prisma.service';
+import { ScheduleService } from 'schedule.service';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaService, ScheduleService],
 })
 export class AppModule {}
